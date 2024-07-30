392c3f7a6b690da6b749418064307c06531cf9a4
shivam chaturvedi <chaturvedishivam598@gmail.com>
2024-07-30 16:57:45 +0530

    vercel.json updated

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install dependencies
 python3 -m pip install -r requirements.txt
+
+# Print the version for debugging
+python3 --version
+
+# Run migrations (optional, should be done post-deployment)
 python3 manage.py makemigrations
 python3 manage.py migrate
+g
